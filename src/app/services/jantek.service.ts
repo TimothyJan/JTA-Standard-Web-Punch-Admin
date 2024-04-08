68fe5516fa185d1779af32888ee8c06ec695ddf9
Timothy jan <jan.timothy@gmail.com>
2024-04-08 14:18:47 -0700

    Issues with server deployment build

--- src/app/services/jantek.service.ts
+++ src/app/services/jantek.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AlertService } from './alert.service';
 import { Observable, Subject } from 'rxjs';
